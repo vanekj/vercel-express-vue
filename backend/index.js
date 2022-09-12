9964c86f5e110095f1e409a017b32d200c7f6f7d
vanekj <info@jakub-vanek.eu>
2022-09-12 16:51:53 +0200

    feat(backend): update application structure

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,20 @@
-var express = require('express');
-var cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
 
-var pokemonsRouter = require('./routes/pokemons');
+const app = express();
 
-var app = express();
-
-var publicPath = path.join(__dirname, 'public');
-var rootHtml = path.join(publicPath, 'index.html');
+const pokemonsRouter = require('./routes/pokemons');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(publicPath));
-
 app.use('/api/pokemons', pokemonsRouter);
-
-// error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json(err);
 });
+
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
--- backend/routes/pokemons.js
+++ backend/routes/pokemons.js
@@ -1,16 +1,16 @@
-var path = require('path');
-var fs = require('fs');
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  var pokemons = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/pokemons.json'), 'utf-8'));
+  const pokemons = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/pokemons.json'), 'utf-8'));
   res.json(pokemons);
 });
 
 router.get('/:id', function(req, res, next) {
-  var pokemons = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/pokemons.json'), 'utf-8'));
-  var pokemon = pokemons.find((pokemon) => pokemon.id === Number(req.params.id));
+  const pokemons = JSON.parse(fs.readFileSync(path.join(__dirname, '../database/pokemons.json'), 'utf-8'));
+  const pokemon = pokemons.find((pokemon) => pokemon.id === Number(req.params.id));
   res.json(pokemon);
 });
 
